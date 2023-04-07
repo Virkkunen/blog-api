f09c65e75f5afb065c700eee93ceba03084e62d9
Virkkunen <vrkknn@vrkknn.net>
2023-04-07 11:25:15 -0300

    req10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+  }, {
+    tableName: 'blog_posts',
+    underscored: true,
+    timestamps: false
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' })
+  };
+  return BlogPost;
+};
